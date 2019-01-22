b72b24450f77be0426a1f68b7d532b262da007e8
Sanditya Srivastava <sanditya2510@gmail.com>
2019-01-22 23:24:55 +0530

    added standard and institution models in core app

--- studylab/core/models.py
+++ studylab/core/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth.base_user import BaseUserManager, AbstractBaseUser
+from django.core.validators import RegexValidator
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+
+class Institution(models.Model):
+    name                = models.CharField(max_length=40)
+    headquarters        = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.name
+
+class Standard(models.Model):
+    SUBJECTS_TYPE = (
+        ('PHY','Physics'),
+        ('CHEM','Chemistry'),
+        ('MATH','Maths'),
+        ('BIOL','Biology')
+    )
+    LANGUAGES = (
+        ('HIN','Hindi'),
+        ('ENG','English'),
+    )
+    subject            = models.CharField(
+        max_length=4, 
+        choices=SUBJECTS_TYPE,
+        verbose_name='Subjects'
+    )
+    language            = models.CharField(
+        max_length=3,
+        choices = LANGUAGES,
+        verbose_name = 'Language',
+    )
+    def __str__(self):
+        return self.subject
