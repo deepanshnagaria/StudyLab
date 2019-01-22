3f05e8367da78aa84c5cd8c49df4948e1a49db80
shreyanshjn <shrey.umt2017@iitr.ac.in>
2019-01-22 23:44:58 +0530

    added standards

--- studylab/core/models.py
+++ studylab/core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Standards(models.Model):
+    STANDARD_TYPE= (
+            ('11th','11th'),
+            ('12th','12th'),
+            ('13th','13th'),
+            )
+    standard_type = models.CharField(choices=STANDARD_TYPE, max_length=10, verbose_name = 'standard type')
+
+    def __str__(self):
+        return self.standard_type
