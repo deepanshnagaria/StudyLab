444eb1cbadfd42d652636dc30730555a8f77e071
shreyanshjn <shrey.umt2017@iitr.ac.in>
2019-01-24 23:36:32 +0530

    created institutions models

--- studylab/Institution/admin.py
+++ studylab/Institution/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
+
+admin.site.register(Center)
+admin.site.register(Batch)
+admin.site.register(Classes)
+admin.site.register(SubjectClasses)
 
 # Register your models here.
