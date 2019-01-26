a550995bf5db503fdf9eac9445421cb5d5a6c98f
unknown <deepansh15111999@gmail.com>
2019-01-26 17:50:58 +0530

    added db.sqlite3 to .gitigmore

--- studylab/core/models.py
+++ studylab/core/models.py
@@ -6,32 +6,34 @@ from django.dispatch import receiver
 
 
 class Institution(models.Model):
-    name                = models.CharField(max_length=40)
-    headquarters        = models.CharField(max_length=40)
+    name = models.CharField(max_length=40)
+    headquarters = models.CharField(max_length=40)
 
     def __str__(self):
         return self.name
 
+
 class Standard(models.Model):
     SUBJECTS_TYPE = (
-        ('PHY','Physics'),
-        ('CHEM','Chemistry'),
-        ('MATH','Maths'),
-        ('BIOL','Biology')
+        ('PHY', 'Physics'),
+        ('CHEM', 'Chemistry'),
+        ('MATH', 'Maths'),
+        ('BIOL', 'Biology')
     )
     LANGUAGES = (
-        ('HIN','Hindi'),
-        ('ENG','English'),
+        ('HIN', 'Hindi'),
+        ('ENG', 'English'),
     )
-    subject            = models.CharField(
-        max_length=20, 
+    subject = models.CharField(
+        max_length=20,
         choices=SUBJECTS_TYPE,
         verbose_name='Subjects'
     )
-    language            = models.CharField(
+    language = models.CharField(
         max_length=20,
-        choices = LANGUAGES,
-        verbose_name = 'Language',
+        choices=LANGUAGES,
+        verbose_name='Language',
     )
+
     def __str__(self):
         return self.subject
